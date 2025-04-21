cd87d8f4702c1e1af1fa64f68a4f81263a1fcc59
Mariano Chouhy <marianochouhy@gmail.com>
2025-04-21 17:38:29 +0200

    end modal button css

--- src/components/EndGameModal.jsx
+++ src/components/EndGameModal.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "../App.css";
 
 const EndGameModal = ({ winner, resetGame }) => {
-  const endGameText = winner === false ? "It's a tie!" : `${winner} wins!`;
+  const endGameText = winner === false ? "It's a tie!" : `Winner is: ${winner}`;
   return (
     <section className="winner">
       <div>
         <span className="text">
           {endGameText}
           <button className="reset-button" onClick={resetGame}>
-            Reset
+            New game
           </button>
         </span>
       </div>
